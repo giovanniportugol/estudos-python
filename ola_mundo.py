fa5dd3366c700541730ef984d05c89508647d7d2
giovanniportugol <giovannisouzinha@gmail.com>
2026-05-17 11:14:09 -0300

    Create ola_mundo.py

---
+++ ola_mundo.py
@@ -0,0 +1,8 @@
+# Primeiro exercício em Python
+# Objetivo: praticar entrada e saída de dados
+
+nome = input("Digite seu nome: ")
+
+print("Olá,", nome)
+print("Seja bem-vindo aos meus estudos de Python!")
+print("Este é meu primeiro exercício no GitHub.")
